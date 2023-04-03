ae770e5d16d6b2d9b20955397957e5807aa5ded9
Gustavo Xavier <gustavohsx07@gmail.com>
2023-04-02 22:15:16 -0300

    adicionar e buscar

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 import socket
 import threading
 
+frases = {}
+
 def main():
 
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -52,9 +54,40 @@ def tratamentoMensagem(cliente, data):
             else:
                 trat = f'{trat}{dados[i]}'
             print(trat)
-        envMenssagem(cliente, f'{tratado}')
+        operacoes(cliente, tratado)
     except:
         return 'Erro ao tratar a mensagem'
-    
+
+
+def operacoes(cliente, dados_tratados):
+
+    tratados = dados_tratados
+    mensagem = ''
+    print(tratados)
+
+    if tratados[0] == 'operacao':
+        if tratados[1] == 'adicionar':
+            print('Realizando a operação: ', tratados[1])
+            try:
+                frases[tratados[3]] = tratados[5]
+                mensagem = '\nAdicionado com sucesso!\n'
+                envMenssagem(cliente, mensagem)
+            except:
+                mensagem = '\n<ERRO> Não foi possivel realizar a operação de adição\n'
+                envMenssagem(cliente, mensagem)
+        
+        elif tratados[1] == 'buscar':
+            print('Realizando a operação: ', tratados[1])
+            try:
+                frase = frases.get(tratados[3])
+                mensagem = f'\nFrase: {frase}\n'
+                envMenssagem(cliente, mensagem)
+            except:
+                mensagem = '\n<ERRO> Não foi possivel realizar a operação de busca\n'
+                envMenssagem(cliente, mensagem)
+    else:
+        mensagem = f'\n<ERRO> Não foi possivel identificar a operação! {tratados[0]}\n'
+        envMenssagem(cliente, mensagem)
+
 
 main()
